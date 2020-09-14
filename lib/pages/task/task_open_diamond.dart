5be39c3b81da8240b04d359ef7071d05dd93a0e4
xuyabing <xy>
2020-09-14 14:13:00 +0800

    支付宝调试

--- lib/pages/task/task_open_diamond.dart
+++ lib/pages/task/task_open_diamond.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_alipay/flutter_alipay.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'dart:io' as H;
 import 'package:fluwx/fluwx.dart' as fluwx;
 
@@ -18,6 +20,7 @@ class _TaskOpenDiamondPageState extends State<TaskOpenDiamondPage> {
   String _url = "https://wxpay.wxutil.com/pub_v2/app/app_pay.php";
 
   String _result = "无";
+
   @override
   void initState() {
     fluwx.weChatResponseEventHandler.listen((res) {
@@ -76,13 +79,12 @@ class _TaskOpenDiamondPageState extends State<TaskOpenDiamondPage> {
                             color: Color(0xFFBC9B87)),
                       ),
                       Container(
-                        margin: EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 15),
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 15),
                         padding: EdgeInsets.symmetric(horizontal: 16),
                         height: 216,
                         decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(16)),
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
                             shape: BoxShape.rectangle,
                             color: Color(0xFFBC9B87)),
                         child: Column(
@@ -126,8 +128,7 @@ class _TaskOpenDiamondPageState extends State<TaskOpenDiamondPage> {
                                   Text(
                                     "VIP会员每条任务1元，钻石会员每条2元。",
                                     style: TextStyle(
-                                        color: Color(0xFF222222),
-                                        fontSize: 14),
+                                        color: Color(0xFF222222), fontSize: 14),
                                   ),
                                 ],
                               ),
@@ -140,34 +141,13 @@ class _TaskOpenDiamondPageState extends State<TaskOpenDiamondPage> {
                   GestureDetector(
                     onTap: () async {
                       //todo 调用接口去开通钻石会员 ，调用微信支付
-                      var h = H.HttpClient();
-                      h.badCertificateCallback = (cert, String host, int port) {
-                        return true;
-                      };
-                      var request = await h.getUrl(Uri.parse(_url));
-                      var response = await request.close();
-                      var data = await Utf8Decoder().bind(response).join();
-                      Map<String, dynamic> result = json.decode(data);
-                      print(result['appid']);
-                      print(result["timestamp"]);
-                      fluwx.payWithWeChat(
-                        appId: result['appid'].toString(),
-                        partnerId: result['partnerid'].toString(),
-                        prepayId: result['prepayid'].toString(),
-                        packageValue: result['package'].toString(),
-                        nonceStr: result['noncestr'].toString(),
-                        timeStamp: result['timestamp'],
-                        sign: result['sign'].toString(),
-                      )
-                          .then((data) {
-                        print("---》$data");
-                      });
+                    callAlipay();
                     },
                     child: Container(
                       height: 46,
                       alignment: Alignment.center,
-                      margin: EdgeInsets.symmetric(
-                          vertical: 65, horizontal: 16),
+                      margin:
+                          EdgeInsets.symmetric(vertical: 65, horizontal: 16),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.all(Radius.circular(46)),
                         gradient: LinearGradient(colors: [
@@ -187,5 +167,54 @@ class _TaskOpenDiamondPageState extends State<TaskOpenDiamondPage> {
           ),
         ) // This trailing comma makes auto-formatting nicer for build methods.
         );
+  }
+
+  Future callWx() async {
+    var h = H.HttpClient();
+    h.badCertificateCallback = (cert, String host, int port) {
+      return true;
+    };
+    var request = await h.getUrl(Uri.parse(_url));
+    var response = await request.close();
+    var data = await Utf8Decoder().bind(response).join();
+    Map<String, dynamic> result = json.decode(data);
+    print(result['appid']);
+    print(result["timestamp"]);
+    fluwx
+        .payWithWeChat(
+      appId: result['appid'].toString(),
+      partnerId: result['partnerid'].toString(),
+      prepayId: result['prepayid'].toString(),
+      packageValue: result['package'].toString(),
+      nonceStr: result['noncestr'].toString(),
+      timeStamp: result['timestamp'],
+      sign: result['sign'].toString(),
+    )
+        .then((data) {
+      print("---》$data");
+    });
+  }
+
+  String _payInfo = "alipay_sdk=alipay-sdk-php-easyalipay-20190926&app_id=2016101400682987&biz_content=%7B%22subject%22%3A+%22%E8%90%8C%E8%B1%A1%E7%94%9F%E6%B4%BB%E5%95%86%E5%93%81%E4%BA%A4%E6%98%93%22%2C%22out_trade_no%22%3A+%22408%22%2C%22timeout_express%22%3A+%2230m%22%2C%22total_amount%22%3A+%2245%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%7D&charset=UTF-8&format=json&method=alipay.trade.app.pay&notify_url=http%3A%2F%2Fmxss.0371.ml%3A88%2Findex.php%3Froute%3Dapp%2Fnotify%2Falipay&sign_type=RSA2&timestamp=2020-09-14+14%3A07%3A43&version=1.0&sign=NuVxhh1U1Bbx%2BaTsNp6cUYMqbId7OLokBe1yGexcoV2biaRbB4dypVUqzAkFVzlGwHbHfrvoAkADsBp%2BZfKu6lxlMKYp7OgBP6cwruA%2FYLAgtLvT0MDgMFzwz%2F%2B9njZY44u4YRsALI6jIL1seDZLSn5FXrAUDq5arwEA16P%2BcaTTS5ymx16fUqx1bTN6y%2BMGuAU7X53ZI37D3nujMEibaH549amYQWrWYxuRb6g%2Fbv%2FedLKjeP7MBozWylFNsBrqStjf1OCoPW2hj8OCYpyNE7eUqkSA3RcB0dNI7pwjhIhf3sRzJaDBAtOainSGCcR1hfv3cVhIX%2FamTSEd4%2Brtvg%3D%3D";
+  String _totalPrice;
+  AlipayResult _payResult;
+
+  callAlipay() async {
+    dynamic payResult;
+    try {
+      print("The pay info is : " + _payInfo);
+      payResult = await FlutterAlipay.pay(_payInfo);
+    } on Exception catch (e) {
+      payResult = null;
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _payResult = payResult;
+      Fluttertoast.showToast(
+          msg: _payResult == null ? "" : _payResult.toString(),
+          textColor: Colors.black);
+    });
   }
 }
