9d10fe45618f2bc0d86613efe6466c30bb6ed498
xuyabing <xy>
2020-10-14 18:11:59 +0800

    添加iOS端相关功能提示

---
+++ lib/pages/task/task_about.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/screenutil.dart';
+import 'package:star/http/api.dart';
+import 'package:star/pages/widget/my_webview.dart';
+
+import '../../global_config.dart';
+
+class AboutPage extends StatefulWidget {
+  @override
+  _AboutPageState createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _buildInfoCard() {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: ScreenUtil().setHeight(40),
+        ),
+        Ink(
+          decoration: BoxDecoration(color: Colors.white),
+          child: InkWell(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (BuildContext context) {
+                return WebViewPage(
+                    initialUrl: APi.AGREEMENT_SERVICES_URL, title: "用户协议");
+              }));
+            },
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "可淘星选用户服务协议",
+                          style: TextStyle(fontSize: ScreenUtil().setSp(42)),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: ScreenUtil().setWidth(32),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Divider(
+          height: 1,
+        ),
+        Ink(
+          decoration: BoxDecoration(color: Colors.white),
+          child: InkWell(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (BuildContext context) {
+                return WebViewPage(
+                  initialUrl: APi.AGREEMENT_PRIVACY_URL,
+                  title: "隐私政策",
+                );
+              }));
+            },
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "可淘星选隐私保护协议",
+                          style: TextStyle(fontSize: ScreenUtil().setSp(42)),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: ScreenUtil().setWidth(32),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+
+//
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "关于我们",
+            style: TextStyle(
+                color: Color(0xFF222222), fontSize: ScreenUtil().setSp(54)),
+          ),
+          leading: IconButton(
+            icon: Image.asset(
+              "static/images/icon_ios_back.png",
+              width: ScreenUtil().setWidth(36),
+              height: ScreenUtil().setHeight(63),
+              fit: BoxFit.fill,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          centerTitle: true,
+          backgroundColor: GlobalConfig.taskNomalHeadColor,
+          elevation: 0,
+        ),
+        body: _buildInfoCard());
+  }
+}
