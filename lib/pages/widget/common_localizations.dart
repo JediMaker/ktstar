1bd96cef840a4976fc89d1c3ef36e9c5b9f4f73e
xuyabing <xy>
2020-11-20 15:46:22 +0800

    兼容ios

--- lib/pages/widget/common_localizations.dart
+++ lib/pages/widget/common_localizations.dart
@@ -78,6 +78,9 @@ class _DefaultCupertinoLocalizations extends CupertinoLocalizations {
   String get copyButtonLabel => "复制";
 
   @override
+  String get modalBarrierDismissLabel => "";
+
+  @override
   String get cutButtonLabel => "剪切";
 
   @override
@@ -145,4 +148,7 @@ class _DefaultCupertinoLocalizations extends CupertinoLocalizations {
   @override
   // TODO: implement todayLabel
   String get todayLabel => throw UnimplementedError();
+
+  @override
+  String tabSemanticsLabel({int tabIndex, int tabCount}) => '';
 }
