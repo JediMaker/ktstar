aa6a6c854faafc152b5c83a5e68db238b3fe4413
xuyabing <xy>
2021-01-23 17:17:48 +0800

    添加首页分类实体

---
+++ lib/generated/json/home_pdd_category_entity_helper.dart
@@ -0,0 +1,63 @@
+import 'package:star/models/home_pdd_category_entity.dart';
+
+homePddCategoryEntityFromJson(HomePddCategoryEntity data, Map<String, dynamic> json) {
+	if (json['status'] != null) {
+		data.status = json['status'];
+	}
+	if (json['err_code'] != null) {
+		data.errCode = json['err_code']?.toInt();
+	}
+	if (json['err_msg'] != null) {
+		data.errMsg = json['err_msg'];
+	}
+	if (json['data'] != null) {
+		data.data = new HomePddCategoryData().fromJson(json['data']);
+	}
+	return data;
+}
+
+Map<String, dynamic> homePddCategoryEntityToJson(HomePddCategoryEntity entity) {
+	final Map<String, dynamic> data = new Map<String, dynamic>();
+	data['status'] = entity.status;
+	data['err_code'] = entity.errCode;
+	data['err_msg'] = entity.errMsg;
+	if (entity.data != null) {
+		data['data'] = entity.data.toJson();
+	}
+	return data;
+}
+
+homePddCategoryDataFromJson(HomePddCategoryData data, Map<String, dynamic> json) {
+	if (json['cats'] != null) {
+		data.cats = new List<HomePddCategoryDataCat>();
+		(json['cats'] as List).forEach((v) {
+			data.cats.add(new HomePddCategoryDataCat().fromJson(v));
+		});
+	}
+	return data;
+}
+
+Map<String, dynamic> homePddCategoryDataToJson(HomePddCategoryData entity) {
+	final Map<String, dynamic> data = new Map<String, dynamic>();
+	if (entity.cats != null) {
+		data['cats'] =  entity.cats.map((v) => v.toJson()).toList();
+	}
+	return data;
+}
+
+homePddCategoryDataCatFromJson(HomePddCategoryDataCat data, Map<String, dynamic> json) {
+	if (json['cat_id'] != null) {
+		data.catId = json['cat_id']?.toInt();
+	}
+	if (json['cat_name'] != null) {
+		data.catName = json['cat_name']?.toString();
+	}
+	return data;
+}
+
+Map<String, dynamic> homePddCategoryDataCatToJson(HomePddCategoryDataCat entity) {
+	final Map<String, dynamic> data = new Map<String, dynamic>();
+	data['cat_id'] = entity.catId;
+	data['cat_name'] = entity.catName;
+	return data;
+}
---
+++ lib/models/home_pdd_category_entity.dart
@@ -0,0 +1,22 @@
+import 'package:star/generated/json/base/json_convert_content.dart';
+import 'package:star/generated/json/base/json_field.dart';
+
+class HomePddCategoryEntity with JsonConvert<HomePddCategoryEntity> {
+	bool status;
+	@JSONField(name: "err_code")
+	int errCode;
+	@JSONField(name: "err_msg")
+	dynamic errMsg;
+	HomePddCategoryData data;
+}
+
+class HomePddCategoryData with JsonConvert<HomePddCategoryData> {
+	List<HomePddCategoryDataCat> cats;
+}
+
+class HomePddCategoryDataCat with JsonConvert<HomePddCategoryDataCat> {
+	@JSONField(name: "cat_id")
+	int catId;
+	@JSONField(name: "cat_name")
+	String catName;
+}
